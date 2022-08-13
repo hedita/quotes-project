cf9dfafcec258d8086ed9095b9e905f6ab81b7c9
hedi-takrami <hedi.takrami@gmail.com>
2022-08-14 03:49:17 +0430

    fix indentation

--- quotes.js
+++ quotes.js
@@ -1,10 +1,11 @@
 let quotes = document.getElementById("quote");
 let author = document.getElementById("author");
 let tags = document.getElementById("tag-item");
-async function getQuotes() {
-        let result = await fetch("https://api.quotable.io/random");
-        let resultJson = await result.json();
-        console.log(resultJson);
-      }
 
-      getQuotes();
+async function getQuotes() {
+  let result = await fetch("https://api.quotable.io/random");
+  let resultJson = await result.json();
+  console.log(resultJson);
+}
+
+getQuotes();
