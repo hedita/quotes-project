03f853e0a95e9c8090b657fedcdf5acc6dd8c22a
hedi-takrami <hedi.takrami@gmail.com>
2022-08-14 03:47:21 +0430

    add variabels

--- quotes.js
+++ quotes.js
@@ -1,4 +1,7 @@
-  async function getQuotes() {
+let quotes = document.getElementById("quote");
+let author = document.getElementById("author");
+let tags = document.getElementById("tag-item");
+async function getQuotes() {
         let result = await fetch("https://api.quotable.io/random");
         let resultJson = await result.json();
         console.log(resultJson);
