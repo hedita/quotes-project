ea167f30cfdffe91fe68b5ff599171016b9334c9
hedi-takrami <hedi.takrami@gmail.com>
2022-08-15 02:47:03 +0430

    add some changes

--- quotes.js
+++ quotes.js
@@ -1,17 +1,19 @@
 const quotes = document.getElementById("quote");
-const author = document.getElementById("author");
+const writer = document.getElementById("author");
 const tags = document.getElementById("tag-item");
+const tagsList = document.getElementById("tag-list");
 
 async function getQuotes() {
   const result = await fetch("https://api.quotable.io/random");
   const resultJson = await result.json();
-  const {content , writer , tags: tagsArray}  = resultJson;
+  const {content , author , tags: tagsArray}  = resultJson;
 
-  quotes.innerHTML = resultJson.content;
-  author.innerHTML = resultJson.author;
+  quotes.innerHTML = content;
+  writer.innerHTML = author;
   tagsArray.forEach(tag => {
     tags.innerHTML = tag;
   });
+console.log(resultJson)
 }
 
 getQuotes();
